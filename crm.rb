5904e5098bae718aa9e70880406048434351f0e6
Paul Walsh <twentytwentyk@live.com>
2017-01-22 21:27:00 -0500

    update both files, still 75% done

---
+++ crm.rb
@@ -0,0 +1,90 @@
+require_relative 'contact'
+
+class CRM
+
+  def initialize(name)
+    @name = name
+  end
+
+  def main_menu
+      while true
+        print_main_menu
+        user_selected = gets.to.i
+        call_option(user_selected)
+      end
+  end
+
+  def print_main_menu
+    puts '[1] Add a new contact'
+    puts '[2] Modify an existing contact'
+    puts '[3] Delete a contact'
+    puts '[4] Display all the contacts'
+    puts '[5] Search by attribute'
+    puts '[6] Exit'
+    puts 'Enter a number: '
+  end
+
+  def call_option(user_selected)
+    case user_selected
+    when 1 then add_new_contact
+    when 2 then modify_existing_contact
+    when 3 then delete_contact
+    when 4 then display_all_contacts
+    when 5 then search_by_attribute
+    when 6 then exit
+    # Finish off the rest for 3 through 6
+    # To be clear, the methods add_new_contact and modify_existing_contact
+    # haven't been implemented yet
+    end
+  end
+
+  def add_new_contact
+  print 'Enter First Name: '
+  first_name = gets.chomp
+
+  print 'Enter Last Name: '
+  last_name = gets.chomp
+
+  print 'Enter Email Address: '
+  email = gets.chomp
+
+  print 'Enter a Note: '
+  note = gets.chomp
+
+  Contact.create(first_name, last_name, email, note)
+
+  end
+  def modify_existing_contact
+
+    print "Enter ID of the contact you'd like to modify:"
+    id = gets.chomp.to_s
+
+    print "Would you like to change the first_name, last_name, email, or note."
+    attribute = gets.chomp.to_s
+
+    print "Enter your modifcation"
+    new_value = gets.chomp.to_s
+
+    modify_this_contact = Contact.find(id)
+
+    modify_this_contact.update()
+  end
+
+  def delete_contact
+
+  end
+
+  def display_all_contacts
+
+  end
+
+  def search_by_attribute
+
+  end
+
+
+end
+
+a_crm_app = CRM.new
+a_crm_app.main_menu
+a_crm_app.print_main_menu
