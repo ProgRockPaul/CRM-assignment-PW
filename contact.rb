33f184b20dc055b8a66f6dd38f84b838c741b930
Paul Walsh <twentytwentyk@live.com>
2017-01-23 16:04:13 -0500

    complete contact.rb with no errors using contact_test.rb

--- contact.rb
+++ contact.rb
@@ -1,7 +1,9 @@
+require './test/contact_test.rb'
+
 class Contact
     attr_accessor :first_name, :last_name, :email, :note
     attr_reader :id
-# 
+#
   @@contacts = []
   @@id = 1
 
@@ -28,73 +30,87 @@ class Contact
 
   # This method should accept an id as an argument
   # and return the contact who has that id
-  def self.find
+
+  def self.find(id)
     @@contacts.each do |contact|
-      return contact if contact.id == id
+      if id == contact.id
+        return contact
+      end
+    end
   end
 
   # This method should allow you to specify
   # 1. which of the contact's attributes you want to update
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
-  def update
+  def update(attribute, value)
+    # puts value
+    # puts value.class
     case attribute
-       when "first_name"
-         self.first_name = new_value
-       when "last_name"
-         self.last_name = new_value
-       when "email"
-         self.email = new_value
-       when "note"
-         self.note = new_value
+    when 'first_name'
+       @first_name = value
+    when 'last_name'
+       @last_name = value
+    when 'email'
+       @email = value
+    when 'note'
+       @note = value
     end
   end
+  #
+  #   puts "What attribute would you like to update? Please enter: first_name, last_name, email, or note. Then enter a new value."
+  #   attribute = gets.chomp
+  #   new_value = gets.chomp
+  #   case attribute
+  #      when "first_name"
+  #        self.first_name = new_value
+  #      when "last_name"
+  #        self.last_name = new_value
+  #      when "email"
+  #        self.email = new_value
+  #      when "note"
+  #        self.note = new_value
+  #   end
+  #   return self
+  # end
 
   # This method should work similarly to the find method above
   # but it should allow you to search for a contact using attributes other than id
   # by specifying both the name of the attribute and the value
   # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
-  def self.find_by(first_name, last_name, email, note, id)
-    case attribute
-      when "first_name"
-        Contact.all.each do |contact|
+  def self.find_by(attribute, value)
+      case attribute
+      when 'first_name'
+        @@contacts.each do |contact|
           if value == contact.first_name
             return contact
           end
         end
-        "No match for first name #{value}"
-      when "last_name"
-        Contact.all.each do |contact|
+        "No match for #{value}"
+      when 'last_name'
+        @@contacts.each do |contact|
           if value == contact.last_name
             return contact
           end
         end
-        "No match for last name #{value}"
-      when "email"
-        Contact.all.each do |contact|
+        "No match for #{value}"
+      when 'email'
+        @@contacts.each do |contact|
           if value == contact.email
             return contact
           end
         end
-        "No match for email #{value}"
-      when "note"
-        Contact.all.each do |contact|
+        "No match for #{value}"
+      when 'note'
+        @@contacts.each do |contact|
           if value == contact.note
             return contact
           end
         end
-        "No match for note #{value}"
-      when "id"
-        Contact.all.each do |contact|
-          if value == contact.id
-            return contact
-          end
-        end
-        "No match for id ##{value}"
-    end
-    "#{attribute} is not valid"
+        "No match for #{value}"
+      end
   end
-
+# when attribute is "first_name" and value is "Betty", find all frist names matching Betty.
   #
   # This method should delete all of the contacts
   def self.delete_all
@@ -102,7 +118,7 @@ class Contact
   end
 
   def full_name
-    return "#{first_name}, #{last_name}"
+    return "#{first_name} #{last_name}"
   end
   #
   # # This method should delete the contact
@@ -113,6 +129,6 @@ class Contact
     @@contacts
   end
 end
-#
-# paul = Contact.new('Paul', 'Walsh', 'twentytwentyk@live.com', 'Local Bard')
-# john = Contact.new('John', 'Petrucci', 'bearucci@gmail.com', 'Local Bear')
+
+paul = Contact.new('Paul', 'Walsh', 'twentytwentyk@live.com', 'Local Bard')
+john = Contact.new('John', 'Petrucci', 'bearucci@gmail.com', 'Local Bear')
